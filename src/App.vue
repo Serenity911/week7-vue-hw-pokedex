92d41378814d39361747b1c0931ad82ae586118c
Serenity911 <silvia.simonassi01@gmail.com>
2020-01-17 17:51:39 +0000

    item can be selected

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template lang="html">
   <div>
-    <pokemon-list :pokemonNameUrl='pokemonNameUrl'/>
+    <pokemon-list :pokemonNameUrl='pokemonNameUrl' :parentSelectedPkmnName='selectedPkmnName'/>
   </div>
 </template>
 
 <script>
 import PokemonList from './components/PokemonList.vue'
+import { eventBus } from './main.js'
 
 export default {
   name: 'app',
   data() {
     return {
       pokemonNameUrl: [],
-      parentSelectedPkmnName: 'bulbasaur'
+      selectedPkmnName: 'bulbasaur'
     }
   },
   mounted() {
-    this.fetchPomekonNames()
+    this.fetchPomekonNames(),
+    eventBus.$on('pokemon-selected', (nameSelected) => this.selectPokemon(nameSelected))
   },
   methods: {
     fetchPomekonNames() {
       fetch('https://pokeapi.co/api/v2/pokemon/?limit=808')
       .then(result => result.json())
       .then(result => this.pokemonNameUrl = result.results )
+    },
+    selectPokemon(nameSelected) {
+      return this.selectedPkmnName = nameSelected
     }
   },
   components: {
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,18 +1,25 @@
 <template lang="html">
   <div>
     <ul>
-      <li v-for="pokemon in pokemonNameUrl" :key="pokemon.name">{{ pokemon.name }}</li>
+      <li v-for="(pokemon, index) in pokemonNameUrl" v-on:click="handleSelection(pokemon.name)"> {{ pokemon.name }} </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { eventBus } from '../main.js'
 export default {
   name: 'pokemon-list',
   props: ['pokemonNameUrl', 'parentSelectedPkmnName'],
   data() {
     return {
       selectedPkmnName: this.parentSelectedPkmnName
+    }
+  },
+  methods: {
+    handleSelection(nameSelected) {
+      this.selectedPkmnName = nameSelected
+      eventBus.$emit('pokemon-selected', nameSelected )
     }
   }
 }
